d55be35835334c3aa6d0607b3261324d6515d6e5
Companies House <chgovuk@cpan.org>
2014-05-22 22:50:15 +0000

    Import of CHGOVUK/Dist-Zilla-PluginBundle-CHGOVUK-0.05 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-PluginBundle-CHGOVUK
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         CHGOVUK/Dist-Zilla-PluginBundle-CHGOVUK-0.05.tar.gz
    gitpan-cpan-author:       CHGOVUK
    gitpan-cpan-maturity:     released

--- lib/Dist/Zilla/PluginBundle/CHGOVUK.pm
+++ lib/Dist/Zilla/PluginBundle/CHGOVUK.pm
@@ -1,13 +1,13 @@
 package Dist::Zilla::PluginBundle::CHGOVUK;
 
-our $VERSION = '0.03';
+our $VERSION = '0.05';
 
 use Moose;
 with 'Dist::Zilla::Role::PluginBundle::Easy',
      'Dist::Zilla::Role::PluginBundle::Config::Slicer';
- 
+
 use namespace::autoclean;
- 
+
 has installer => (
     is => 'ro',
     isa => 'Str',
@@ -21,55 +21,57 @@ has prereqs => (
     lazy => 1,
     default => sub { $_[0]->payload->{prereqs} || 'AutoPrereqs' },
 );
- 
+
 sub build_file {
     my $self = shift;
     $self->installer =~ /MakeMaker/ ? 'Makefile.PL' : 'Build.PL';
 }
- 
+
 sub configure {
     my $self = shift;
- 
+
     my @accepts = qw( MakeMaker MakeMaker::IncShareDir ModuleBuild ModuleBuildTiny );
     my %accepts = map { $_ => 1 } @accepts;
- 
+
     unless ($accepts{$self->installer}) {
         die sprintf("Unknown installer: '%s'. " .
                     "Acceptable values are MakeMaker, ModuleBuild and ModuleBuildTiny\n",
                     $self->installer);
     }
- 
+
     my @dirty_files = ('dist.ini', 'Changes', 'META.json', 'README.md', $self->build_file);
     my @exclude_release = ('README.md');
- 
+
     $self->add_plugins(
         [ 'NameFromDirectory' ],
- 
+
         # Make the git repo installable
         [ 'Git::GatherDir', { exclude_filename => [ $self->build_file, 'META.json', 'LICENSE', @exclude_release ] } ],
-        #[ 'CopyFilesFromBuild', { copy => [ 'META.json', 'LICENSE', $self->build_file ] } ],
- 
+        [ 'CopyFilesFromBuild', { copy => [ 'META.json', 'LICENSE', $self->build_file ] } ],
+
         # should be after GatherDir
-        [ 'Git::NextVersion', { first_version => '0.01', version_regexp => '^(.+)$' } ],
- 
+        # Equivalent to Module::Install's version_from, license_from and author_from
+        [ 'VersionFromModule' ],
+        #[ 'LicenseFromModule', { override_author => 1 } ],
+
         [ 'ReversionOnRelease', { prompt => 1 } ],
- 
+
         # after ReversionOnRelease for munge_files, before Git::Commit for after_release
         [ 'NextRelease', { format => '%-v  %{yyyy-MM-dd}d' } ],
- 
+
         [ 'Git::Check', { allow_dirty => \@dirty_files } ],
- 
+
         # Make Github center and front
         [ 'GithubMeta', { issues => 1 } ],
         [ 'ReadmeAnyFromPod', { type => 'markdown', filename => 'README.md', location => 'root' } ],
- 
+
         # Set no_index to sensible directories
         [ 'MetaNoIndex', { directory => [ qw( t xt inc share eg examples ) ] } ],
- 
+
         [ $self->prereqs ],
         [ $self->installer ],
         [ 'MetaJSON' ],
- 
+
         # standard stuff
         [ 'PodSyntaxTests' ],
         [ 'MetaYAML' ],
@@ -80,12 +82,12 @@ sub configure {
         [ 'ShareDir' ],
         [ 'Manifest' ],
         [ 'ManifestSkip' ],
- 
+
         [ 'CheckChangesHasContent' ],
         [ 'TestRelease' ],
         [ 'ConfirmRelease' ],
         [ 'FakeRelease' ],
- 
+
         [ 'CopyFilesFromRelease', { match => '\.pm$' } ],
         [ 'Git::Commit', {
             commit_msg => '%v',
@@ -94,35 +96,35 @@ sub configure {
         } ],
         [ 'Git::Tag', { tag_format => '%v', tag_message => '' } ],
         [ 'Git::Push', { remotes_must_exist => 0 } ],
- 
+
     );
 }
- 
+
 __PACKAGE__->meta->make_immutable;
 1;
- 
+
 __END__
- 
+
 =head1 NAME
- 
+
 Dist::Zilla::PluginBundle::CHGOVUK - Dist::Zilla plugin defaults for Companies House
- 
+
 =head1 SYNOPSIS
- 
+
   ; dist.ini
   name = Dist-Name
   [@CHGOVUK]
   installer = ModuleBuildTiny
- 
+
 =head1 DESCRIPTION
- 
+
 This is a Dist::Zilla plugin bundle that implements the opinionated build
 process of Companies House. Roughly equivalent to:
- 
+
   # TBD
- 
+
 =head1 SEE ALSO
- 
+
 L<Dist::Milla>
- 
+
 =cut
